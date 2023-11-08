6e07196a9e534b3061c896307fcdfa054c5007c2
Bright Bediako <info.brytetech@gmail.com>
2023-11-08 20:54:16 +0000

    Initial commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,107 @@
+import React, { useContext } from 'react';
+import './sidebar.scss';
+import {
+    DashboardIcon,
+    PersonOutlineIcon,
+    LocalShippingIcon,
+    CreditCardIcon,
+    StoreIcon,
+    InsertChartIcon,
+    SettingsApplicationsIcon,
+    ExitToAppIcon,
+    NotificationsNoneIcon,
+    SettingsSystemDaydreamOutlinedIcon,
+    PsychologyOutlinedIcon,
+    AccountCircleOutlinedIcon
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { DarkModeContext } from '../../context/darkModeContext';
+
+const Sidebar = () => {
+    const { dispatch } = useContext(DarkModeContext);
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">React Admin </span>
+                </Link>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <DashboardIcon className="icon" />
+                        <Link to="/" style={{ textDecoration: "none" }}>
+                            <span>Dashboard</span>
+                        </Link>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li>
+                            <PersonOutlineIcon className="icon" />
+                            <span>Users</span>
+                        </li>
+                    </Link>
+                    <Link to="/products" style={{ textDecoration: "none" }}>
+                        <li>
+                            <StoreIcon className="icon" />
+                            <span>Products</span>
+                        </li>
+                    </Link>
+                    <li>
+                        <CreditCardIcon className="icon" />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon className="icon" />
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <InsertChartIcon className="icon" />
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsNoneIcon className="icon" />
+                        <span>Notifications</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydreamOutlinedIcon className="icon" />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyOutlinedIcon className="icon" />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsApplicationsIcon className="icon" />
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountCircleOutlinedIcon className="icon" />
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <ExitToAppIcon className="icon" />
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <div
+                    className="colorOption"
+                    onClick={() => dispatch({ type: "LIGHT" })}
+                ></div>
+                <div
+                    className="colorOption"
+                    onClick={() => dispatch({ type: "DARK" })}
+                ></div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
